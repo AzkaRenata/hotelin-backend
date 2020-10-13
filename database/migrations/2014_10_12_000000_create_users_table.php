3f6f922a72f5821c88e663f5e91d7140ec4b34a1
Muhammad Mujir <muhammadmujir011@gmail.com>
2020-10-13 15:42:26 +0700

    migration table
    migration tabel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->tinyInteger('user_level');
+            $table->enum('gender', ['male', 'female']);
+            $table->string('telp',15);
+            $table->string('address',15);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2020_10_13_062824_create_facility_category_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacilityCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facility_category', function (Blueprint $table) {
+            $table->id();
+            $table->string('facility_name',150);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facility_category');
+    }
+}
