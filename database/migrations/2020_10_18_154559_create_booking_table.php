96fdc44b9bb2b599762e66dd0829f21307163f37
Muhammad Mujir <muhammadmujir011@gmail.com>
2020-10-18 23:36:13 +0700

    Final Database Migration

---
+++ database/migrations/2020_10_18_154559_create_booking_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('room_id')->constrained('room');
+            $table->tinyInteger('booking_status');
+            $table->dateTime('check_in', 0);
+            $table->dateTime('check_out', 0);
+            $table->timestamp('booking_time', 0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking');
+    }
+}
